71a0ea8e24f89a13d1022bc66d90c687689095cc
Stuart Kent <skent.dev@gmail.com>
2019-11-28 21:59:29 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,25 @@
+# Advent Of Code 2019
+
+Kotlin template for [Advent of Code](http://adventofcode.com).
+
+# Instructions
+
+## Running Tests
+
+All days:
+
+    ./gradlew test
+
+Specific day, e.g. 14:
+
+    ./gradlew day14:test
+
+## Verifying Solutions
+
+All days:
+
+    ./gradlew run
+
+Specific day, e.g. 14:
+
+    ./gradlew day14:run
---
+++ day20/build.gradle.kts
@@ -0,0 +1,33 @@
+plugins {
+    kotlin("jvm")
+    application
+}
+
+repositories {
+    jcenter()
+}
+
+dependencies {
+    implementation(kotlin("stdlib-jdk8"))
+    testImplementation("org.junit.jupiter:junit-jupiter-api:5.5.2")
+    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.5.2")
+}
+
+tasks {
+    compileKotlin {
+        kotlinOptions.jvmTarget = "1.8"
+    }
+
+    compileTestKotlin {
+        kotlinOptions.jvmTarget = "1.8"
+    }
+
+    test {
+        useJUnitPlatform()
+        testLogging.showStandardStreams = true
+    }
+}
+
+application {
+    mainClassName = "MainKt"
+}
